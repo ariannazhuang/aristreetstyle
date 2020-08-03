a2e63fdf1582cc8dbf2e52ade86ac9196e95a72f
ariannazhuang <56565052+ariannazhuang@users.noreply.github.com>
2020-08-02 17:52:42 -0700

    Update STYLIST_draft.py

--- STYLIST_draft.py
+++ STYLIST_draft.py
@@ -1,33 +1,44 @@
 import sqlite3
+import time
 
 # database name to be passed as parameter 
 conn = sqlite3.connect('stylist.db')
+c = conn.cursor()
 
 # Create new table with ID as an integer and primary key and Name as a varchar
-conn.execute("create table if not exists items (id integer primary key, image_url varchar(800), item_category varchar(165), style varchar(165), color varchar(165), brand varchar (165), season varchar (165), temperature_min int, temperature_max int, date timestamp)")
+c.execute("create table if not exists items (id integer primary key, item_category varchar(165), style varchar(165), color varchar(165), brand varchar (165), season varchar (165), temperature_min int, temperature_max int, date timestamp)")
 
-conn.execute("create table if not exists weather (id integer primary key, date timestamp, zipcode int, season varchar(165), temperature int, temperature_max int)")
+c.execute("create table if not exists weather (id integer primary key, date timestamp, city varchar(200), temperature int, temperature_feel_like int)")
 
-conn.execute("create table if not exists outfit (id integer primary key, image1 int, image2 int, image3 int, image4 int, image5 int, email varchar(765), timestamp timestamp)")
+c.execute("create table if not exists outfits (id integer primary key, items_id_1 int, items_id_2 int, items_id_3 int, items_id_4 int, items_id_5 int, email varchar(765), timestamp timestamp)")
 
-# Insert
-conn.execute("insert into items (image_url, item_category,style, color, brand, season, temperature_min, temperature_max, date) values('https://veronicabeard.com/collections/new/products/dakota-dress-2?color=Olive', 'Dress', 'MiniDress', 'AnimalPrint','VeronicaBeard', 'SS', 65, 80, (SELECT datetime('unixepoch', 'localtime')))")
+# Insert Items
+c.execute("INSERT INTO items (item_category,style, color, brand, season, temperature_min, temperature_max, date) values('Dress', 'Cocktail', 'Neon_Green','JonathanSimkhai', 'SS', 45, 68, (SELECT datetime('unixepoch', 'localtime')))")
+c.execute("INSERT INTO items (item_category,style, color, brand, season, temperature_min, temperature_max, date) values('Dress', 'SweaterDress', 'Floral_Black','DerekLam', 'FW', 50, 65, (SELECT datetime('unixepoch', 'localtime')))")
+c.execute("INSERT INTO items (item_category,style, color, brand, season, temperature_min, temperature_max, date) values('Top', 'Statement', 'Leopard','Nicholas', 'SS', 55, 70, (SELECT datetime('unixepoch', 'localtime')))")
+c.execute("INSERT INTO items (item_category,style, color, brand, season, temperature_min, temperature_max, date) values('Romper', 'Boho', 'Floral','Zimmermann', 'SS', 58, 75, (SELECT datetime('unixepoch', 'localtime')))")
+c.execute("INSERT INTO items (item_category,style, color, brand, season, temperature_min, temperature_max, date) values('Top', 'Blouse', 'Lavender', 'Nanushka', 'FW', 40, 60, (SELECT datetime('unixepoch', 'localtime')))")
+c.execute("INSERT INTO items (item_category,style, color, brand, season, temperature_min, temperature_max, date) values('Jeans', 'Boyfriend', 'Washed','CalvinKlein', 'FW', 32, 60, (SELECT datetime('unixepoch', 'localtime')))")
 
+#Insert Weather
+from pyowm.owm import OWM
+owm = OWM('0cd868497ee607acfbe7c6d23f18d414')
+city=input('Enter City Name')
+mgr = owm.weather_manager()
+weather = mgr.weather_at_place(city).weather
+temp_dict_kelvin = weather.temperature()   # a dict in Kelvin units (default when no temperature units provided)
+temp_dict_kelvin['temp_min']
+temp_dict_kelvin['temp_max']
+temp_dict_fahrenheit = weather.temperature('fahrenheit')  # a dict in Fahrenheit units
 
-#test 
-print ('Your Outfit Today: ')
-cursor = conn.execute("SELECT image_url FROM items")
+c.execute("INSERT INTO weather (date, city, temperature, temperature_feel_like) VALUES (?, ?, ?, ?)",
+         (time.time(), city, temp_dict_fahrenheit['temp'], temp_dict_fahrenheit['feels_like']))
+
+Commit
+conn.commit()
+
+cursor = conn.execute("SELECT * FROM weather")
 for row in cursor:
     print(row)
-
-#showImage
-import urllib.request
-from PIL import Image
-import requests
-from io import BytesIO
-url = 'https://veronicabeard.com/collections/new/products/dakota-dress-2?color=Olive'
-response = requests.get(url)
-img = Image.open(BytesIO(response.content))
-
 
 conn.close()
